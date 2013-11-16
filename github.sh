f42112f44af8c74ed9942fab598db48ce781ebb4
daniw <dani.winz@bluewin.ch>
2013-11-17 00:11:50 +0100

    fosa

---
+++ github.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Script for configuring my preinstalled ubuntu
+
+# creating structure for studies
+cd ~
+mkdir studium
+cd studium
+mkdir fosa
+mkdir lg-hslu
+mkdir sem1
+mkdir sem2
+mkdir sem3
+# fosa
+cd ~/studium/fosa
+git clone http://daniw@github.com/fosa/fosaet
+git clone http://daniw@github.com/fosa/fosamath
+git clone http://daniw@github.com/fosa/fosaphy
+git clone http://daniw@github.com/fosa/fosastoc
+git clone http://daniw@github.com/fosa/fosatm
