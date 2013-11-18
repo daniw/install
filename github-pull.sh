85beec21c37688eb8d76e16ed6e3668df0beeacc
daniw <dani.winz@bluewin.ch>
2013-11-18 21:23:36 +0100

    Script for updating all my github repos

---
+++ github-pull.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# Script for updating all my recently used git repos
+
+# fosa
+cd ~/studium/fosa/fosaet
+git pull
+cd ~/studium/fosa/fosamath
+git pull
+cd ~/studium/fosa/fosaphy
+git pull
+cd ~/studium/fosa/fosastoc
+git pull
+cd ~/studium/fosa/fosatm
+git pull
+# lg-hslu
+cd ~/studium/lg-hslu/lg-hslu.github.com
+git pull
+# semester 1
+cd ~/studium/sem1
+# semester 2
+cd ~/studium/sem2/et2
+git pull
+cd ~/studium/sem2/ingto
+git pull
+cd ~/studium/sem2/kontt2
+git pull
+cd ~/studium/sem2/stoc
+git pull
+# semester 3
+cd ~/studium/sem3/et-elo
+git pull
+cd ~/studium/sem3/maphy2
+git pull
+cd ~/studium/sem3/prg1
+git pull
+# repos from ninux
+cd ~/studium/ninux/et2-lab
+git pull
+cd ~/studium/ninux/ingtools
+git pull
+cd ~/studium/ninux/prg1
+git pull
+cd ~/studium/ninux/stoc
+git pull
+
+# my own or luxeria projects
+cd ~/electronic/prj/e-wall
+git pull
+cd ~/electronic/prj/install
+git pull
+cd ~/electronic/prj/latex-document
+git pull
+cd ~/electronic/prj/msp430helloworld
+git pull
+cd ~/electronic/prj/pc-gen
+git pull
+cd ~/electronic/prj/py-uLCD
+git pull
+cd ~/electronic/prj/slimsata
+git pull
+cd ~/electronic/prj/stepperdriver
+git pull
+cd ~/electronic/prj/turningtabledriver
+git pull
+cd ~/electronic/prj/tvbgone
+git pull
+cd ~/electronic/prj/xlr_pegelwandler
+git pull
+
+# luxeria repos
+cd ~/luxeria/ict-days.github.com
+git pull
+cd ~/luxeria/luxeria.github.com
+git pull
+cd ~/luxeria/luxlab
+git pull
+cd ~/luxeria/luxpage
+git pull
+cd ~/luxeria/luxstat
+git pull
+cd ~/luxeria/luxtex
+git pull
