11954ccb2e568bfa8e248313dce10bc46785dabc
daniw <daniel.winz@stud.hslu.ch>
2013-12-28 23:37:36 +0100

    printout of actual repository during processing

--- github-pull.sh
+++ github-pull.sh
@@ -2,87 +2,126 @@
 # Script for updating all my recently used git repos
 
 # fosa
+echo fosaet
 cd ~/studium/fosa/fosaet
 git pull
+echo fosamath
 cd ~/studium/fosa/fosamath
 git pull
+echo fosaphy
 cd ~/studium/fosa/fosaphy
 git pull
+echo fosastoc
 cd ~/studium/fosa/fosastoc
 git pull
+echo fosatm
 cd ~/studium/fosa/fosatm
 git pull
+echo fosaweb
 cd ~/studium/fosa/fosaweb
 git pull
+echo kugprg
 cd ~/studium/fosa/kugprg
 git pull
+echo titlepage
 cd ~/studium/fosa/titlepage
 git pull
 # lg-hslu
+echo lg-hslu
 cd ~/studium/lg-hslu/lg-hslu.github.com
 git pull
 # semester 1
 cd ~/studium/sem1
 # semester 2
+echo et2
 cd ~/studium/sem2/et2
 git pull
+echo ingto
 cd ~/studium/sem2/ingto
 git pull
+echo kontt2
 cd ~/studium/sem2/kontt2
 git pull
+echo stoc
 cd ~/studium/sem2/stoc
 git pull
 # semester 3
+echo et-elo
 cd ~/studium/sem3/et-elo
 git pull
+echo maphy2
 cd ~/studium/sem3/maphy2
 git pull
+echo prg1
 cd ~/studium/sem3/prg1
 git pull
 # repos from ninux
+echo ninux/et2-lab
 cd ~/studium/ninux/et2-lab
 git pull
+echo ninux/ingtools
 cd ~/studium/ninux/ingtools
 git pull
+echo ninux/prg1
 cd ~/studium/ninux/prg1
 git pull
+echo ninux/stoc
 cd ~/studium/ninux/stoc
 git pull
 
 # my own or luxeria projects
+echo e-wall
 cd ~/ele/prj/e-wall
 git pull
+echo install
 cd ~/ele/prj/install
 git pull
+echo latex-document
 cd ~/ele/prj/latex-document
 git pull
+echo msp430helloworld
 cd ~/ele/prj/msp430helloworld
 git pull
+echo pc-gen
 cd ~/ele/prj/pc-gen
 git pull
+echo py-uLCD
 cd ~/ele/prj/py-uLCD
 git pull
+echo slimsata
 cd ~/ele/prj/slimsata
 git pull
+echo stepperdriver
 cd ~/ele/prj/stepperdriver
 git pull
+echo turningtabledriver
 cd ~/ele/prj/turningtabledriver
 git pull
+echo tvbgone
 cd ~/ele/prj/tvbgone
 git pull
+echo xlr_pegelwandler
 cd ~/ele/prj/xlr_pegelwandler
 git pull
 
 # luxeria repos
+echo ict-days
 cd ~/luxeria/ict-days.github.com
 git pull
+echo luxeria
 cd ~/luxeria/luxeria.github.com
 git pull
+echo luxlab
 cd ~/luxeria/luxlab
 git pull
+echo luxpage
 cd ~/luxeria/luxpage
 git pull
+echo luxstat
 cd ~/luxeria/luxstat
 git pull
+echo luxtex
 cd ~/luxeria/luxtex
 git pull
+
+echo finished
