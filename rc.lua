61f4dac56bd403f32ead63541e4b48d0d2f986a1
daniw <dani.winz@bluewin.ch>
2015-12-15 20:09:14 +0100

    removes unused menues

--- rc.lua
+++ rc.lua
@@ -82,14 +82,14 @@ end
 -- Create a laucher widget and a main menu
 myawesomemenu = {
    { "manual", terminal .. " -e man awesome" },
-   { "edit config", editor_cmd .. " " .. awesome.conffile },
+   --{ "edit config", editor_cmd .. " " .. awesome.conffile },
    { "restart", awesome.restart },
-   { "quit", awesome.quit }
+   --{ "quit", awesome.quit }
 }
 
 mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-                                    { "Debian", debian.menu.Debian_menu.Debian },
-                                    { "open terminal", terminal }
+                                    --{ "Debian", debian.menu.Debian_menu.Debian },
+                                    --{ "open terminal", terminal }
                                   }
                         })
 
